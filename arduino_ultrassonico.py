317e4dad5d40a47f768b41639ec4ebd48c5f9fe4
Roberto Guedes <robertoguedes@edu.univali.br>
2024-08-16 14:52:35 -0300

    adicionado descrições

--- arduino_ultrassonico.py
+++ arduino_ultrassonico.py
@@ -2,18 +2,23 @@
 # acionar a bomba que vai aumentar essa distância (bombeando a água para fora) até 190cm, que será o gatilho para
 # parar a bomba. Se essa distância chegar a menos de 120cm um alarme será acionado até que o nível volte para uma
 # distância maior que 150cm.
-from time import sleep
+from time import sleep  # Para colocar pausas entre as medições, evitar sobrecarregar o sistema.
 
+# Calibragem dos limites de distâncias entre o sensor e o nível d'água
 aciona_bomba = 150
 aciona_alarme = 120
 para_bomba = 190
 para_alarme = aciona_bomba
+
+# Variáveis que mostram se a bomba ou alarme estão acionados
 bomba = bool()
 alarme = bool()
 
-
+# Programa em loop mantido enquanto o arduino ficar ligado
 while True:
-    distancia = int(input("Distância do nível d'água: "))
+    sleep(2)  # Pausa
+    distancia = int(input("Distância do nível d'água: "))  # Lê o sinal
+# Abaixo seguem as condicionais de avaliação
     if distancia > aciona_alarme:
         print('Alarme OFF')
         alarme = bool(False)
@@ -26,7 +31,9 @@ while True:
     else:
         print('Bomba ON')
         bomba = bool(True)
+# Abaixo o loop que mantém alarme e/ou bomba ligados enquanto o nível não baixar para o limite de parar
     while alarme or bomba:
+        sleep(2)
         distancia = int(input("Distância do nível d'água: "))
         if alarme:
             if distancia < para_alarme:
